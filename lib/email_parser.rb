2b9a549571fe058e6b359da1ee206f2660a14bc1
Omayma Mahjoub <omayma.mahjoub@ensi-uma.tn>
2020-07-13 16:16:11 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,15 +14,11 @@ class EmailAddressParser
       m.split(",")
       if m.is_a?(Array)
         m.each do |singlem|
-          if singlem.end_with?(",")
-            singlem=singlem.delete_suffix(",")
-          end
+          singlem=singlem.delete_suffix(",")
           arr2<<singlem
         end
       else
-        if m.end_with?(",")
-          m=m.delete_suffix(",")
-        end
+        m=m.delete_suffix(",")
         arr2<<m
       end
     end
